9b483a2ce544106d446529ad06b5ecc175390ec1
vogdb <vozhdb@gmail.com>
2018-02-17 12:03:47 +0300

    correct build

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ ! -f $NESTML_INSTALL_DIR/nestml.jar ]; then
     echo "Please set a correct NESTML_INSTALL_DIR" 1>&2
     exit 1
 fi
-java -jar $NESTML_INSTALL_DIR/nestml.jar research_team_models/ --target build/
+java -jar $NESTML_INSTALL_DIR/nestml.jar neuron_simulator_models/ --target build/
 
 cd build
 cmake -Dwith-nest=$NEST_INSTALL_DIR/bin/nest-config .
